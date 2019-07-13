9e2260db6c87f4ca53b31bed9f9cc75d465f3b9b
Herverson <herverson.sousa@gmail.com>
2019-07-12 23:08:42 -0300

    Inicial

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Todo List</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-PmY9l28YgO4JwMKbTvgaS7XNZJ30MK9FAZjjzXtlqyZCqBY6X6bXIkM++IkyinN+" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="container">
+      @yield('content')
+    </div>
+
+    <footer id="footer" class="text-center">
+      <p>Copyright &copy; 2019 Todo List</p>
+    </footer>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js" integrity="sha384-vhJnz1OVIdLktyixHY4Uk3OHEwdQqPppqYR8+5mjsauETgLOcEynD9oPHhhz18Nw" crossorigin="anonymous"></script>
+  </body>
+</html>
